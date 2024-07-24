5387643b75ba9d63c211f1d10947a882a6f19724
Darya Petrenko <144802569+DaryPet@users.noreply.github.com>
2024-07-24 10:12:46 +0200

    Initial commit

---
+++ task-02.py
@@ -0,0 +1,21 @@
+import re
+from typing import Callable
+
+
+def generator_numbers(text: str):
+    number_pattern = re.compile(r'\b\d+\.\d+\b')
+    for match in number_pattern.finditer(text):
+        yield float(match.group())
+
+
+
+def sum_profit(text: str, func: Callable[[str], any]) -> float:
+    return sum(func(text))
+
+
+
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
---
+++ task-03.py
@@ -0,0 +1,3 @@
+def parse_log_line(line: str) -> dict:
+    parts = line.split(" ", 3)
+    print(parts)
